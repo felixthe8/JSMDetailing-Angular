e82be9d0ab8e3f4da08bbbb3b247ba3f20b806fe
Felix Huang <felixhuang@Felixs-MacBook-Pro.local>
2020-03-01 12:41:19 -0800

    Adding the source images from the original project
    Defined the banner component for the homepage

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { HeaderComponent } from './shared-components/header/header.component';
-import { NavBarComponent } from './shared-components/nav-bar/nav-bar.component'
+import { NavBarComponent } from './shared-components/nav-bar/nav-bar.component';
+import { BannerComponent } from './homepage/banner/banner.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     HomepageComponent,
     HeaderComponent,
-    NavBarComponent
+    NavBarComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
