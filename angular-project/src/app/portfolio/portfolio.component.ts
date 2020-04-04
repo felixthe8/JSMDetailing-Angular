cd2c9721d1e9b4f8ef9e181111c9bf56157a3fb0
Felix H <felixthe8@gmail.com>
2020-04-03 23:32:52 -0700

    Utilize Material Icon component in Portfolio. Remove Font Awesome

--- angular-project/src/app/portfolio/portfolio.component.ts
+++ angular-project/src/app/portfolio/portfolio.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Portfolio } from 'src/assets/portfolio';
-import { faCamera } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-portfolio',
@@ -15,7 +14,6 @@ export class PortfolioComponent implements OnInit {
   portfolioHeader = 'JSM Portfolio';
   portfolioHeaderText = `This page shows images from our last few detailing jobs. We update this page every week
                               with new images, so check back soon!`;
-  faCamera = faCamera;
 
   ngOnInit(): void {
     this.portfolioImageList = Portfolio.images;
